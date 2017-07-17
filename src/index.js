39438a3762a5ddefa78d594af571adfe574196eb
Matt Magoffin <matt@solarnetwork.net>
2017-07-17 15:34:31 +1200

    Tweak into flat ES6 module to support normal imports.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import * as domain from './domain/domain';
-import * as format from './format/format';
-import * as net from './net/net';
-import * as util from './util/util';
-
-export { 
-    domain, 
-    format, 
-    net, 
-    util 
-};
+export * from './domain/domain';
+export * from './format/format';
+export * from './net/net';
+export * from './util/util';
