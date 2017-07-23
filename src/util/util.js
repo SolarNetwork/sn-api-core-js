6611dbca86276f781f2787a2c4684f95056246b4
Matt Magoffin <matt@solarnetwork.net>
2017-07-23 20:57:17 +1200

    Add application logger API with simple console output backend.

--- src/util/util.js
+++ src/util/util.js
@@ -3,5 +3,6 @@
 export { default as ComparableEnum } from './comparableEnum';
 export { default as Configuration } from './configuration';
 export { default as Enum } from './enum';
+export { default as Logger } from './logger';
 export { default as MultiMap } from './multiMap';
 export { default as PropMap } from './propMap';
