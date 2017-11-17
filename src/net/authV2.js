fd5aa519f6a0878ec4255bd321fda716a20394ee
Matt Magoffin <matt@solarnetwork.net>
2017-11-17 21:46:08 +1300

    Add support for wss: and ws: protocols when setting URL.

--- src/net/authV2.js
+++ src/net/authV2.js
@@ -166,7 +166,8 @@ class AuthorizationV2Builder {
     url(url) {
         const uri = uriParse(url);
         let host = uri.host;
-        if ( uri.port && ((uri.scheme === 'https' && uri.port !== 443) || (uri.scheme === 'http' && uri.port !== 80)) ) {
+        if ( uri.port && (((uri.scheme === 'https' || uri.scheme === 'wss') && uri.port !== 443) 
+                || ((uri.scheme === 'http' || uri.scheme === 'ws') && uri.port !== 80)) ) {
             host += ':' + uri.port;
         }
         if ( uri.query ) {
