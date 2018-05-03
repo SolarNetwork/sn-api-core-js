2d7fb768ccb797185e40b716b2c057683869efbc
Matt Magoffin <matt@solarnetwork.net>
2018-05-03 20:40:37 +1200

    Tweak export namespace.

--- src/util/util.js
+++ src/util/util.js
@@ -7,4 +7,4 @@ export { default as Logger,
  logLevels } from './logger';
 export { default as MultiMap } from './multiMap';
 export { default as PropMap } from './propMap';
-export { default as date } from './date';
+export { default as dateUtils } from './date';
