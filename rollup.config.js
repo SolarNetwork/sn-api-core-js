d22f7d644b1f9cd29f03e4d9c74d39b8414b401f
Matt Magoffin <matt@solarnetwork.net>
2019-08-27 13:03:57 +1200

    Update to more modern deps (Ava 1.4, Babel 7, and Rollup 1.20) to fix GitHub security warnings.

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,48 @@
-import babel from 'rollup-plugin-babel';
-import includePaths from 'rollup-plugin-includepaths';
+import babel from "rollup-plugin-babel";
+import includePaths from "rollup-plugin-includepaths";
 
 const includePathOptions = {
-  include: {},
-  paths: ['src'],
-  external: [],
-  extensions: ['.js']
+	include: {},
+	paths: ["src"],
+	external: [],
+	extensions: [".js"]
 };
 
 export default {
-  external: id => {
-    return /(crypto-js|d3-|uri-js)/.test(id);
-  },
-  globals: {
-    'd3-array': 'd3',
-    'd3-collection': 'd3',
-    'd3-time': 'd3',
-    'd3-time-format': 'd3',
-    'crypto-js/enc-base64':'CryptoJS.Base64',
-    'crypto-js/enc-hex': 'CryptoJS.Hex',
-    'crypto-js/hmac-sha256': 'CryptoJS.HmacSHA256',
-    'crypto-js/sha256': 'CryptoJS.SHA256',
-    'uri-js': 'URI',
-  },
-  plugins: [
-    includePaths(includePathOptions),
-    babel({
-      exclude: 'node_modules/**',
-      babelrc: false,
-      plugins: ['external-helpers'],
-      presets: [
-        ['env', {
-          targets: {
-            browsers: ['last 2 versions'],
-            node: 'current',
-          },
-          modules: false,
-        }]
-      ]
-    })]
+	external: id => {
+		return /(crypto-js|d3-|uri-js)/.test(id);
+	},
+	output: {
+		globals: {
+			"d3-array": "d3",
+			"d3-collection": "d3",
+			"d3-time": "d3",
+			"d3-time-format": "d3",
+			"crypto-js/enc-base64": "CryptoJS.Base64",
+			"crypto-js/enc-hex": "CryptoJS.Hex",
+			"crypto-js/hmac-sha256": "CryptoJS.HmacSHA256",
+			"crypto-js/sha256": "CryptoJS.SHA256",
+			"uri-js": "URI"
+		}
+	},
+	plugins: [
+		includePaths(includePathOptions),
+		babel({
+			exclude: "node_modules/**",
+			babelrc: false,
+			plugins: [],
+			presets: [
+				[
+					"@babel/env",
+					{
+						targets: {
+							browsers: ["last 2 versions"],
+							node: "current"
+						},
+						modules: false
+					}
+				]
+			]
+		})
+	]
 };
