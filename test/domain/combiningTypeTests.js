911829501eb4e1007bb15d787f27516b89bfe972
Matt Magoffin <matt@solarnetwork.net>
2020-05-01 10:14:02 +1200

    NET-249: add support for Difference combining type.

--- test/domain/combiningTypeTests.js
+++ test/domain/combiningTypeTests.js
@@ -1,14 +1,15 @@
-import test from 'ava';
+import test from "ava";
 
-import {default as CombiningTypes, CombiningType } from 'domain/combiningType';
+import { default as CombiningTypes, CombiningType } from "domain/combiningType";
 
-test('domain:combiningType:create', t => {
-	const obj = new CombiningType('foo', 1);
-    t.truthy(obj);
-    t.is(obj.name, 'foo');
+test("domain:combiningType:create", t => {
+	const obj = new CombiningType("foo", 1);
+	t.truthy(obj);
+	t.is(obj.name, "foo");
 });
 
-test('domain:combiningType:aggregations', t => {
-	t.is(CombiningTypes.Average.name, 'Average');
-	t.is(CombiningTypes.Sum.name, 'Sum');
+test("domain:combiningType:constants", t => {
+	t.is(CombiningTypes.Average.name, "Average");
+	t.is(CombiningTypes.Difference.name, "Difference");
+	t.is(CombiningTypes.Sum.name, "Sum");
 });
