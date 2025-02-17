6d78c956a92c19a664f2b7144eca419e7cb5680c
Matt Magoffin <matt@solarnetwork.net>
2025-02-18 09:34:51 +1300

    Build latest.

--- lib/util/arrays.d.ts
+++ lib/util/arrays.d.ts
@@ -12,4 +12,12 @@ export declare function pushValues<T>(result: T[], values?: Iterable<T>): void;
  * @returns a new array of the lower-cased and sorted items
  */
 export declare function lowercaseSortedArray(items: string[]): string[];
+/**
+ * Create a set that contains values that occur in two different sets.
+ *
+ * @param s1 the first set (or array)
+ * @param s2  the second set (or array)
+ * @returns a new set with only values that occur in both `s1` and `s2`
+ */
+export declare function intersection<T>(s1?: Set<T> | T[], s2?: Set<T> | T[]): Set<T>;
 //# sourceMappingURL=arrays.d.ts.map
---
+++ lib/util/jsonEncodable.d.ts
@@ -0,0 +1,15 @@
+/**
+ * API for an object that can encode itself into a JSON-ready structure.
+ * @public
+ */
+export default interface JsonEncodable {
+    /**
+     * Get this object in standard JSON form.
+     *
+     * This method will return a "plain" object, suitable for passing to `JSON.stringify()`.
+     *
+     * @return an object, ready for JSON encoding
+     */
+    toJsonObject(): Record<string, any>;
+}
+//# sourceMappingURL=jsonEncodable.d.ts.map
